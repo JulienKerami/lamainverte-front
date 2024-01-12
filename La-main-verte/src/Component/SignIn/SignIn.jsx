f830fc93849143177906190b2e0f5dc7abbb1ee2
Marty Goron <martygoron@gmail.com>
2024-01-12 14:58:26 +0100

    message mdp add

--- La-main-verte/src/Component/SignIn/SignIn.jsx
+++ La-main-verte/src/Component/SignIn/SignIn.jsx
@@ -14,6 +14,7 @@ function Signin(props) {
   const [EmailError, setEmailError] = useState(false)
   const [Password1Error, setPassword1Error] = useState(false)
   const [Password2Error, setPassword2Error] = useState(false)
+  const [ResetPassword, setResetPassword] = useState("Mot de passe")
 
   useEffect(() => {
     return () => {
@@ -37,12 +38,13 @@ function Signin(props) {
 
   const handleSubmit = async (e) => {
 
-        setFirstNameError(false)
+          setFirstNameError(false)
           setLastNameError(false)
           setEmailError(false)
           setPassword1Error(false)
           setPassword2Error(false)
-
+          setResetPassword("")
+            
     let UserArray = {
       lastname: e.target[0].value,
       firstname: e.target[1].value,
@@ -55,19 +57,12 @@ function Signin(props) {
    
     //La regex permets d'utiliser une method nativ à nodeJS qui permets de verifier si certains caractères sont présent dans une chaîne de caractères.
 
-    //////////////////////////////////////////            REGEX   ///////////////////////////////////////////////
-
+      //REGEX  
     const EmailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
-
     const NameRegex = /^[A-Z][-a-zA-Z]+$/
-
-    // const PasswordRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
-   
-
-
-
     const PasswordRegEx = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/gm;
       
+
 
     if (NameRegex.test(e.target[0].value) === false) {
       console.log("false firstName")
@@ -75,11 +70,11 @@ function Signin(props) {
       setFirstNameError(true)
     }
   
-     if (NameRegex.test(e.target[1].value) === false) {
+    if (NameRegex.test(e.target[1].value) === false) {
         console.log("false LastName")
        validation = false
        setLastNameError(true)
-       }
+     }
   
      if (EmailRegEx.test(e.target[2].value) === false) {
       
@@ -89,12 +84,25 @@ function Signin(props) {
      
     }
 
-     if (PasswordRegEx.test(e.target[3].value) === false)
-    {
-      console.log("password not strong enough");
-      validation = false
-      setPassword1Error(true)
-     
+    if (e.target[3].value.length < 8) {
+      console.log("doit contenir au moins 8 caractères");
+      setError("doit contenir au moins 8 caractères")
+      setResetPassword("")
+    }
+
+    if (!/(?=.\d)/.test(e.target[3].value)) {
+      console.log("doit contenir au moins un chiffre");
+      setError("doit contenir au moins un chiffre");
+    }
+
+    if (!/(?=.[A-Z])/.test(e.target[3].value)) {
+      console.log("doit contenir au moins une lettre majuscule")
+      setError("doit contenir au moins une lettre majuscule")
+    }
+
+    if (!/(?=.[!@#$%^&*(),.?":{}|<>])/.test(e.target[3].value)) {
+      console.log("doit contenir au moins un caractère spécial")
+      setError("doit contenir au moins un caractère spécial")
     }
 
      if (!(e.target[3].value === e.target[4].value) )
@@ -103,8 +111,6 @@ function Signin(props) {
       validation = false
       setPassword2Error(true)
     }
- 
-  
 
     if (validation === true) { 
       console.log("subscribed!");
@@ -112,7 +118,6 @@ function Signin(props) {
       const user = await createUser(UserArray)
   }
    
-
     // if (user.request.status === 400) {
     //   alert(user.response.data)
     //   return
@@ -121,7 +126,7 @@ function Signin(props) {
     // alert(`Bienvenue ${UserArray.firstname}`)
   }
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
 
   return (
     <>
@@ -140,8 +145,8 @@ function Signin(props) {
           {lastNameError?<span className='NameMessage' style={{ margin: "0", padding: "0" }}>Veuillez rentrer un prénom valide</span>:null}
           <input type="text" placeholder='Email' />
           {EmailError?<span className='NameMessage' style={{ margin: "0", padding: "0" }}>Veuillez rentrer un email valide</span>:null}
-          <input type="password" placeholder='Mot de passe' />
-          {Password1Error?<span className='NameMessage' style={{ margin: "0", padding: "0" }}>Votre mot de passe n'est pas assez robuste</span>:null}
+          <input type="password" placeholder={ResetPassword} />
+          {Password1Error?<span className='NameMessage' style={{ margin: "0", padding: "0" }}>{error}</span>:null}
           <input type="password" placeholder='Confirmer mot de passe' />
           {Password2Error?<span className='NameMessage' style={{ margin: "0", padding: "0" }}>Votre mot de passe ne correspond pas</span>:null}
           <button>Sign in</button>
@@ -152,5 +157,6 @@ function Signin(props) {
 }
 
 export default Signin;
+
 
 
