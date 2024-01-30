9b22a86ee4e35d64887eacba947acb71f362e5e6
Mathieu Flores <mathieu.flores18@gmail.com>
2024-01-30 12:38:24 +0100

    title fixed

--- La-main-verte/src/Component/Home/Home.jsx
+++ La-main-verte/src/Component/Home/Home.jsx
@@ -26,11 +26,11 @@ function Home(props) {
     return (
         <>
         <section className='home'>
-           {localStorage.name?
+           
 
-           <h2 className='home_texte1'>Faites pousser vos légumes en toute sérénité. {userInfos.lastname}</h2>:null}
+           <h2 className='home_texte1'>Faites pousser vos légumes en toute sérénité. {userInfos.lastname}</h2>
            <p className='home_texte2'>Quand semer ? Quand récolter ? Comment entretenir son potager ? Des semis à la récolte, la main verte vous aide à chaque étape.</p>
-           <Link to='/signin' className='home__inscription-button'>Inscription</Link>
+           {!localStorage.name?<Link to='/signin' className='home__inscription-button'>Inscription</Link>:null}
            <img src="fiche.png" alt="icone fiche" className='icone_fiche' /> 
            <p className='home_texte3'>une to-do list personalisé</p>
            <img src="camera.png" alt="icone camera" className='icone_camera' /> 
